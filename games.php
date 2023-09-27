d4bff1c83738e84dbe0d783a3fc06310246e3883
randilyngomme <122951455+randilyngomme@users.noreply.github.com>
2023-09-27 14:39:10 -0500

    Update games.php

--- games.php
+++ games.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-games.php")
   
-$pageTitle="games";
+$pageTitle="game";
 include "view-header.php";
 $Games = selectGames();
 include "view-games.php";
