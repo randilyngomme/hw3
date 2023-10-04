9768be85f713dc740a43520461a864d68d6e659d
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-04 07:19:33 -0500

    Update model-player.php

--- model-player.php
+++ model-player.php
@@ -2,7 +2,7 @@
 function selectPlayer() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT player_id,player_name,player_number FROM `player` ");
+        $stmt = $conn->prepare("SELECT player_id,player_name,player_gender FROM `player` ");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
