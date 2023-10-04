f9cd011cad4d5c20ec748511a0ff6916de943270
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-04 09:24:10 -0500

    Update player-of-game.php

--- player-of-game.php
+++ player-of-game.php
@@ -4,7 +4,7 @@ require_once("model-player-of-game.php");
   
 $pageTitle ="player of game";
 include "view-header.php";
-$PlayerOfGame = selectPlayerOfGame($_GET['id']);
+$PlayerOfGames = selectPlayerOfGame($_GET['id']);
 include "view-player-of-game.php";
 include "view-footer.php";
 ?>
