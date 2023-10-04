6813d37eb86db06737b54b9a90a0f5241f4a285e
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-04 12:07:36 -0500

    Update model-profile-of-player.php

--- model-profile-of-player.php
+++ model-profile-of-player.php
@@ -2,7 +2,7 @@
 function selectProfileOfPlayer($pid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT p.player_id,player_name,player_gender,platform,device FROM `player` p join profile pf on pf.player_id= pf.player_id WHERE pf.player_id=? ");
+        $stmt = $conn->prepare("SELECT p.player_id,player_name,player_gender,platform,device FROM `player` p join profile pf on p.player_id= pf.player_id WHERE pf.player_id=? ");
         $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
