4c4dc82acda76188bf16843b010c96d06e88eca3
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-04 06:50:31 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -28,6 +28,9 @@
         <li class="nav-item">
           <a class="nav-link" href="games-with-players.php">Games with players</a>
         </li>
+         <li class="nav-item">
+          <a class="nav-link" href="DLC.php">DLC</a>
+        </li>
       </ul>
     </div>
   </div>
