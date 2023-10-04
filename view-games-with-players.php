1cc4babc36e17614ea4544e62511715c88c15617
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-04 12:20:37 -0500

    Update view-games-with-players.php

--- view-games-with-players.php
+++ view-games-with-players.php
@@ -7,14 +7,16 @@ while($game = $Games->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $game['game_name']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
 <?php
         $Players= selectGameWithPlayer($game['game_id']);
         while($player=$Players->fetch_assoc()){
           ?>
+             <li class="list-group-item"><?php echo $player['player_gender']; ?> -<?php echo $player['platform']; ?>- <?php echo $player['device']; ?></li>
           <?php
         }
 ?>
-        
+        </ul>
         </p>
       <p class="card-text"><small class="text-body-secondary">Number:<?php echo $game['game_number']; ?> </small></p>
     </div>
