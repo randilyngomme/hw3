80cc09c5b48faa0375a26fee76060bd70a396dc0
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-04 07:52:25 -0500

    Create view-player-of-game.php

---
+++ view-player-of-game.php
@@ -0,0 +1,29 @@
+<h1>Player of game</h1>
+<div class="table-responsive">
+  <table class="table">
+  <thead>
+    <tr>
+       <th>ID</th>
+    <th>Name</th>
+    <th>Gender</th>
+      <th>platform</th>
+      <th>device</th>
+    </tr>
+  </thead>
+    <tbody>
+<?php
+while($player = $player->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $player['player_id']; ?></td>
+    <td><?php echo $player['player_name']; ?></td>
+    <td><?php echo $player['player_gender']; ?></td>
+    <td><?php echo $player['platform']; ?></td>
+    <td><?php echo $player['device']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
