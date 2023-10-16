6e051d71efdf4e56ff264aa576d34ec68ee0ace7
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-16 17:47:43 -0500

    Update players.php

--- players.php
+++ players.php
@@ -4,6 +4,18 @@ require_once("model-player.php");
   
 $pageTitle ="player";
 include "view-header.php";
+
+if (isset($_POST['actionType']))
+{
+  switch($_POST['actionType'])
+    {
+      case "Add":
+      InsertPlayer{$_POST['pName'], $_POST['pGender']
+        break;
+    }
+}
+
+  
 $Players = selectPlayer();
 include "view-player.php";
 include "view-footer.php";
