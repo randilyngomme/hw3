165bd5c6688bb966366bb18186f0969828aa2f6f
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-23 15:02:34 -0500

    Update view-games-with-players-newform.php

--- view-games-with-players-newform.php
+++ view-games-with-players-newform.php
@@ -18,31 +18,20 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-               <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
+       <form>
+           <div class="mb-3">
+             <label for="exampleInputEmail1" class="form-label">Email address</label>
+             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+           </div>
+           <div class="mb-3">
+             <label for="exampleInputPassword1" class="form-label">Password</label>
+             <input type="password" class="form-control" id="exampleInputPassword1">
+           </div>
+      
          
-        
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
+           <button type="submit" class="btn btn-primary">Submit</button>
+         </form>
       </div>
     </div>
   </div>
