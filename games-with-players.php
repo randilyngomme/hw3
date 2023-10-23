7d94af6c2d11056440f942b9b9c66e5a2d5db8a8
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-23 17:13:53 -0500

    Update games-with-players.php

--- games-with-players.php
+++ games-with-players.php
@@ -8,14 +8,12 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertGamesWithPlayer($_POST['gameid'],$_POST['playerid'],$_POST['platform'],$_POST['device'],$_POST['genre']))
-      {
+      if (insertGamesWithPlayer($_POST['gameid'],$_POST['playerid'],$_POST['platform'],$_POST['device'],$_POST['genre'])) {
         echo '<div class="alert alert-success" role="alert"> Player profile added!</div>';
-        ) else{
+        ) else {
         echo '<div class="alert alert-danger" role="alert"> ERROR!</div>';
       }
                break;
-      }
                        
   }
 }
