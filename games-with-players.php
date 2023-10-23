23db58e2100816e68ba7dd63832e92c0d7124355
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-23 16:56:17 -0500

    Update games-with-players.php

--- games-with-players.php
+++ games-with-players.php
@@ -4,6 +4,16 @@ require_once("model-games-with-players.php");
   
 $pageTitle ="games with players";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertGamesWithPlayer($_POST['gid'],$_POST['pid'],$_POST['platform'],$_POST['device'],$_POST['genre']);
+                            break;
+  }
+}
+
+
 $Games = selectGames();
 include "view-games-with-players.php";
 include "view-footer.php";
