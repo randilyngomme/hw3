f874b76777774a5694e9e1e7558f372f3aa6fb6c
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-23 15:01:13 -0500

    Update view-games-with-players-newform.php

--- view-games-with-players-newform.php
+++ view-games-with-players-newform.php
@@ -25,9 +25,11 @@
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Password</label>
               <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" class="form-control" id="exampleInputPassword1">
             </div>
             <div class="mb-3">
               <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
             </div>
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Password</label>
