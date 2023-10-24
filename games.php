0c5376555e3afbe5298539838eae249630604b2e
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-24 12:27:01 -0500

    Update games.php

--- games.php
+++ games.php
@@ -4,6 +4,15 @@ require_once("model-games.php");
   
 $pageTitle ="games";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add";
+    insertGames($_POST['gName'],$_POST['gNumber'])
+      break;
+  }
+}
+
 $Games = selectGames();
 include "view-games.php";
 include "view-footer.php";
