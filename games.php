783ce13d53b94a8a5a817f9bbcf40a313e19c3a4
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-24 12:38:25 -0500

    Update games.php

--- games.php
+++ games.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add";
-    insertGames($_POST['gName'],$_POST['gNumber']);
+    if(insertGames($_POST['gName'],$_POST['gNumber'])){
+      echo "<div class="alert alert-success" role='alert">Game Added!</div>';
+    } else{
+       echo "<div class="alert alert-danger" role='alert">ERROR!</div>';
+    }
       break;
   }
 }
