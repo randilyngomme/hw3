53f33b79370cf5d4ca7fda7ef376e3bab2342f3c
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-24 16:41:18 -0500

    Update DLC.php

--- DLC.php
+++ DLC.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add";
-    if(insertGames($_POST['dlcName'],$_POST['dlcNumber'])){
+    if(insertDLC($_POST['dlcName'],$_POST['dlcNumber'])){
       echo '<div class="alert alert-success" role="alert">DLC Added!</div>';
     } else{
        echo '<div class="alert alert-danger" role="alert">ERROR!</div>';
