90ad33930870c41482b052a81bc28109341f347b
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-24 16:51:47 -0500

    Update DLC.php

--- DLC.php
+++ DLC.php
@@ -17,6 +17,15 @@ if (isset($_POST['actionType'])) {
   }
 }
 
+   case "Delete":
+      if (deletePlayer($_POST['dlcid'])){
+        echo '<div class="alert alert-success" role="alert">DLC deleted!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+        break;
+    }
+}
 
 $DLC = selectDLC();
 include "view-DLC.php";
