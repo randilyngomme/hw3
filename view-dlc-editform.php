5d7b492e88ab760d469048743b737a69b1d17ed0
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-24 17:08:36 -0500

    Update view-dlc-editform.php

--- view-dlc-editform.php
+++ view-dlc-editform.php
@@ -1,35 +1,34 @@
-
-
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newDlcModal">
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editDlcModal<?php echo $dlc['dlc_id']; ?>">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
 </svg>
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="newDlcModal" tabindex="-1" aria-labelledby="newDlcModalLabel" aria-hidden="true">
+<div class="modal fade" id="editDlcModal<?php echo $dlc['dlc_id']; ?>" tabindex="-1" aria-labelledby="editDlcModalLabel<?php echo $dlc['dlc_id']; ?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newDlcModalLabel">New DLC</h1>
+        <h1 class="modal-title fs-5" id="editDlcModalLabel<?php echo $dlc['dlc_id']; ?>">Edit DLC</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form method="post" action="">
-            <div class="mb-3">
-              <label for="dlcName" class="form-label">DLC Name</label>
-              <input type="text" class="form-control" id="dlcName" name="dlcName">
-            </div>
-               <div class="mb-3">
-              <label for="dlcNumber" class="form-label">DLC Number</label>
-              <input type="text" class="form-control" id="dlcNumber" name="dlcNumber">
-            </div>
-          <input type="hidden" name="actionType" value="Add">
-            <button type="submit" class="btn btn-primary">Save</button>
-          </form>
-      </div>
+  <div class="mb-3">
+    <label for="dlcName<?php echo $dlc['dlc_id']; ?>" class="form-label">DLC Name</label>
+    <input type="text" class="form-control" id="dlcName<?php echo $dlc['dlc_id']; ?>" name="dlcName" value="<?php echo $dlc['dlc_name']; ?>">
+  </div>
+   <div class="mb-3">
+    <label for="dlcNumber<?php echo $dlc['dlc_id']; ?>" class="form-label">DLC Number</label>
+    <input type="text" class="form-control" id="dlcNumber<?php echo $dlc['dlc_id']; ?>" name="dlcNumber" value="<?php echo $dlc['dlc_Number']; ?>">
+  </div>
+          <input type="hidden" name="dlcid" value="<?php echo $dlc['dlc_id']; ?>">
+       <input type="hidden" name="actionType" value="Edit">
+  <button type="submit" class="btn btn-primary">Save</button>
+</form>
+    
     </div>
   </div>
 </div>
