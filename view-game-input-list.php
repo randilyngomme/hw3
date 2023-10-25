99d16c5775e107a43524c2e2283ee1c5c89e42d4
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-25 01:32:14 -0500

    Update view-game-input-list.php

--- view-game-input-list.php
+++ view-game-input-list.php
@@ -2,8 +2,12 @@
   <option selected>Open this select menu</option>
 <?php 
 while($gameItem= $gameList->fetch_assoc()){
+  $selText="";
+  if($selectedGames==$gameItem['game_id']){
+    $selText="selected";
+  }
   ?>
-   <option value="<?php echo $gameItem['game_id'];?>"><?php echo $gameItem['game_name'];?></option>
+   <option value="<?php echo $gameItem['game_id'];?>"<?=$selText?> ><?php echo $gameItem['game_name'];?></option>
   <?php
 }
 ?>
