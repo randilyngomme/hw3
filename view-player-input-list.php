3502f3447c71ff6d438d9645c4e4cbc879f72733
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-25 01:52:57 -0500

    Update view-player-input-list.php

--- view-player-input-list.php
+++ view-player-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="player_id" name="player_id">
 <?php 
 while($playerItem= $playerList->fetch_assoc()){
+  $selText="";
+  if($selectedPlayer==$playerItem['player_id']){
+    $selText=" selected";
+  }
   ?>
-   <option value="<?php echo $playerItem['player_id']; ?>"><?php echo $playerItem['player_id']; ?></option>
+   <option value="<?php echo $playerItem['player_id']; ?>"<?=$selText?>><?php echo $playerItem['player_name']; ?></option>
   <?php
 }
 ?>
