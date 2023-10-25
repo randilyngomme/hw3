c25a219a92dc1b1a6700f4a2e55e3c8a10193e2d
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-10-24 19:47:50 -0500

    Update view-games-with-players.php

--- view-games-with-players.php
+++ view-games-with-players.php
@@ -22,11 +22,7 @@ while($game = $Games->fetch_assoc()) {
         while($player=$Players->fetch_assoc()){
           ?>
              <li class="list-group-item"><?php echo $player['player_name']; ?>-<?php echo $player['player_gender']; ?> -<?php echo $player['platform']; ?>- <?php echo $player['device']; ?></li>
-          <?
-
-
-
-            
+         
               <form method="post" action="">
           <input type="hidden" name="gwpid" value="<?php echo $game['gwpid']; ?>">
           <input type="hidden" name="actionType" value="Delete">
