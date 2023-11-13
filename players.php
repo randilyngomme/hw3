84e77749b7dae83a839cd3506a30da495a031e2b
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-11-13 14:22:44 -0600

    Update players.php

--- players.php
+++ players.php
@@ -3,7 +3,40 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.8.0/sweetalert2.min.css" rel="stylesheet"/>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.8.0/sweetalert2.all.min.js"></script>
 
+
+    <!-- Animation -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
 <?php
+
+
+    <div style="margin:20px;">
+        <button id="btn2" class="btn btn-success">Rotate</button>
+        <div id="d2" class="sty1" style="display:flex;flex-direction:row;justify-content:center;align-items:center;">
+            DIV
+        </div>
+
+        <script>
+            "use strict";
+            {
+                let degree = 0;
+                let btn = document.querySelector("#btn2");
+                btn.addEventListener("click", () =>
+                {
+                    degree = degree + 10;
+                    let div = document.querySelector("#d2");
+                    let params = {
+                        targets: div,
+                        rotate: { value: degree, duration: 5000 }
+
+                    };
+
+                    anime(params);
+
+                });
+            }
+        </script>
+    </div>
+
 require_once("util-db.php");
 require_once("model-player.php");
   
@@ -24,9 +57,9 @@ if (isset($_POST['actionType']))
           text: "success",
           icon: "success",
           });
-
         </script>
         <?php
+        
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
