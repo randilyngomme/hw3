3134d2b47dee65bb4dcb5adcadfc73b7482472a3
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-11-13 17:47:45 -0600

    Update view-player.php

--- view-player.php
+++ view-player.php
@@ -1,44 +1,11 @@
-
 <div class="row">
-  <div class ="col">
+  <div class="col">
     <h1>Players</h1>
   </div>
-    <div class="col-auto">
-<?php
-include "view-player-newform.php";
-?>
-    </div>
+  <div class="col-auto">
+    <?php include "view-player-newform.php"; ?>
+  </div>
 </div>
-
-<div class="table-responsive">
-  <table class="table">
-  <thead>
-    <tr>
-       <th>ID</th>
-    <th>Name</th>
-    <th>Gender</th>
-      <th></th>
-      <th></th>
-      <th></th>
-    </tr>
-  </thead>
-    <tbody>
-<?php
-while($player = $Players->fetch_assoc()) {
-?>
-  <tr>
-    <td><?php echo $player['player_id']; ?></td>
-    <td><?php echo $player['player_name']; ?></td>
-    <td><?php echo $player['player_gender']; ?></td>
-    <td>
-      <?php
-    include "view-player-editform.php";
-      ?>
-    </td>
-
-
-
-    
 
 <style>
   .rotate-square {
@@ -53,18 +20,45 @@ while($player = $Players->fetch_assoc()) {
   }
 </style>
 
- 
-    
-   <td>
-  <div class="rotate-square"></div>
-  <button id="btnturn" type="Test" class="btn btn-danger" onClick="rotateSquare(); ">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
-      <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
-      <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
-    </svg>
-  </button>
-</td>
-
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Gender</th>
+        <th></th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      while ($player = $Players->fetch_assoc()) {
+      ?>
+        <tr>
+          <td><?php echo $player['player_id']; ?></td>
+          <td><?php echo $player['player_name']; ?></td>
+          <td><?php echo $player['player_gender']; ?></td>
+          <td>
+            <?php include "view-player-editform.php"; ?>
+          </td>
+          <td>
+            <div class="rotate-square"></div>
+            <button id="btnturn" type="Test" class="btn btn-danger" onClick="rotateSquare(); ">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+                <!-- SVG path here -->
+              </svg>
+            </button>
+          </td>
+          <!-- Other cells here -->
+        </tr>
+      <?php
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
 
 <script>
   function rotateSquare() {
