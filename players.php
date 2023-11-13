e88601947cd071d0ecdfa8949b544c53c2f0086f
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-11-13 14:24:58 -0600

    Update players.php

--- players.php
+++ players.php
@@ -4,39 +4,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.8.0/sweetalert2.all.min.js"></script>
 
 
-    <!-- Animation -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
 <?php
-
-
-    <div style="margin:20px;">
-        <button id="btn2" class="btn btn-success">Rotate</button>
-        <div id="d2" class="sty1" style="display:flex;flex-direction:row;justify-content:center;align-items:center;">
-            DIV
-        </div>
-
-        <script>
-            "use strict";
-            {
-                let degree = 0;
-                let btn = document.querySelector("#btn2");
-                btn.addEventListener("click", () =>
-                {
-                    degree = degree + 10;
-                    let div = document.querySelector("#d2");
-                    let params = {
-                        targets: div,
-                        rotate: { value: degree, duration: 5000 }
-
-                    };
-
-                    anime(params);
-
-                });
-            }
-        </script>
-    </div>
-
 require_once("util-db.php");
 require_once("model-player.php");
   
