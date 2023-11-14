15e75faee0f5b4a3d517d8d07125b5512262b1c9
Randilyn <122951455+randilyngomme@users.noreply.github.com>
2023-11-14 14:51:40 -0600

    Update view-player.php

--- view-player.php
+++ view-player.php
@@ -1,4 +1,50 @@
 
+
+   
+   <td>
+  <div class="rotate-square"></div>
+  <button id="btnturn" type="hidden" class="btn btn-danger" onClick="rotateSquare(); ">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+      <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
+      <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
+    </svg>
+  </button>
+</td>
+
+
+    
+
+<style>
+  .rotate-square {
+    width: 30px;
+    height: 20px;
+    background-color: purple;
+    transition: transform 0.5s ease-in-out;
+  }
+
+  .rotate-square.rotate {
+    transform: rotate(360deg);
+  }
+</style>
+
+ 
+ 
+
+
+<script>
+  function rotateSquare() {
+    var square = document.querySelector('.rotate-square');
+    square.classList.toggle('rotate');
+  }
+</script>
+
+
+
+
+
+
+
+
 <div class="row">
   <div class ="col">
     <h1>Players</h1>
@@ -36,42 +82,6 @@ while($player = $Players->fetch_assoc()) {
       ?>
     </td>
 
-
-
-    
-
-<style>
-  .rotate-square {
-    width: 30px;
-    height: 20px;
-    background-color: purple;
-    transition: transform 0.5s ease-in-out;
-  }
-
-  .rotate-square.rotate {
-    transform: rotate(360deg);
-  }
-</style>
-
- 
-    
-   <td>
-  <div class="rotate-square"></div>
-  <button id="btnturn" type="hidden" class="btn btn-danger" onClick="rotateSquare(); ">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
-      <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
-      <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
-    </svg>
-  </button>
-</td>
-
-
-<script>
-  function rotateSquare() {
-    var square = document.querySelector('.rotate-square');
-    square.classList.toggle('rotate');
-  }
-</script>
 
 
 
